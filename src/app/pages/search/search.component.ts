5d9565faf5afa4f21d879ec29f9930c03c9672e6
Julia Taro <julia.taro@gmail.com>
2025-07-19 23:18:11 +0300

    Added Components and Service

---
+++ src/app/pages/search/search.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit } from "@angular/core";
+import { MasterService } from "../../service/master.service";
+import { Observable } from "rxjs";
+import { AsyncPipe, DatePipe } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: "app-search",
+  standalone: true,
+  imports: [AsyncPipe, FormsModule, DatePipe, RouterLink],
+  templateUrl: "./search.component.html",
+  styleUrl: "./search.component.css",
+})
+export class SearchComponent implements OnInit {
+  locations$: Observable<any[]> = new Observable<any[]>();
+  masterSrv = inject(MasterService);
+
+  ngOnInit(): void {
+    this.getAllLocations();
+  }
+
+  getAllLocations() {
+    this.locations$ = this.masterSrv.getLocations();
+  }
+}
---
+++ src/app/service/master.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class MasterService {
+  apiURl: string = "https://projectapi.gerasim.in/api";
+
+  constructor(private http: HttpClient) {}
+
+  getLocations(): Observable<any[]> {
+    return this.http.get<any[]>("${this.baseUrl}/BusBooking/GetBusLocations");
+  }
+}
